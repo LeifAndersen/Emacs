99927598dfbe5d62a5319736690d30a7b7385588
Leif Andersen <leif@leifandersen.net>
2012-10-20 10:30:05 -0600

    Updated the install.sh file.

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ cp .viper ~/.viper
 cp -r .emacs.d ~/.emacs.d
 tar xvf geiser-0.2.1.tar.gz -C ~/
 tar xvf cedet-1.1.tar.gz -C ~/
+tar xvf emacs-jabber-0.8.91.tar.gz -C ~/
 cd ~/cedet-1.1
 make
-
+cd ~/emacs-jabber-0.8.91
+./configure
+make
