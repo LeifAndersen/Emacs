2062333b3280294f93dd3848dd34959b3946d449
Leif Andersen <leif@leifandersen.net>
2012-11-22 23:15:53 -0700

    Updated .emacs and install.sh too

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ tar xvf cedet-1.1.tar.gz -C ~/
 tar xvf emacs-jabber-0.8.91.tar.gz -C ~/
 tar xvf magit-1.2.0.tar.gz -C ~/
 unzip -o nxhtml-2.08-100425.zip -d ~/
+tar xvf ensime-master.tar.gz -C ~/.emacs.d
 cd ~/cedet-1.1
 make
 cd ~/magit-1.2.0
